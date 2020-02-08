4d5fe371b98fd4fca06a60eb1e12fc3a77835c8c
Andre Luiz Pedro Botelho <andrepedrobotelho@gmail.com>
2020-02-07 21:11:32 -0300

    test nigthwatch

---
+++ tests/login.test.js
@@ -0,0 +1,31 @@
+module.exports = {
+  /*'login com sucesso': function (browser) {
+    browser
+      .url('http://zombie-web:5000/login')
+      .waitForElementVisible('.card-login', 3000)
+      .setValue('input[name=email]', 'zumbi@hotmail.com')
+      .setValue('input[name=password]', 'pwd123')
+      .click('.login-button')
+      .waitForElementVisible('.user .info span', 3000)
+      .assert.containsText('.user .info span', 'Andre')
+      .end();
+  },*/
+  'quando eu acesso a pagi na de login': function (browser) {
+    browser
+      .url('http://zombie-web:5000/login')
+      .waitForElementVisible('.card-login', 3000)
+  },
+  'e faço login com sucesso': function (browser) {
+    browser
+      .setValue('input[name=email]', 'zumbi@hotmail.com')
+      .setValue('input[name=password]', 'pwd123')
+      .click('.login-button')
+  },
+  'então devo ver o nome na área logada': function (browser) {
+    var userInfo = '.user .info span'
+    browser
+      .waitForElementVisible(userInfo, 3000)
+      .assert.containsText(userInfo, 'Andre')
+      .end();
+  },
+}
